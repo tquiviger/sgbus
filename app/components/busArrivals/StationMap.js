43e1b036ca05256e4195f777ce38a06f0b116e03
thomas <tquiviger@gmail.com>
2016-05-13 15:16:57 +0800

    GooglsMapsApiKey in configuration file

--- app/components/busArrivals/StationMap.js
+++ app/components/busArrivals/StationMap.js
@@ -2,14 +2,14 @@ var React = require('react');
 var GoogleMap = require('google-map-react')
 var StationMarker = require('./mapMarkers/StationMarker')
 var BusMarker = require('./mapMarkers/BusMarker')
-
+var Config = require('Config')
 
 function StationMap(props) {
     return (
         <GoogleMap
-            bootstrapURLKeys={{  key: 'AIzaSyAhcYZVvNqQNkT8chx5FCVbErNI4YCUB0c',    language: 'fr'  }}
-            center={{lat: props.stationData.stationDesc.Latitude, lng: props.stationData.stationDesc.Longitude}}
-            defaultZoom={15}>
+            bootstrapURLKeys={{ key: Config.GoogleMapsApiKey, language: 'fr' }}
+            center={{ lat: props.stationData.stationDesc.Latitude, lng: props.stationData.stationDesc.Longitude }}
+            defaultZoom={ 15 }>
             {
                 props.stationData.Services.map(function (result) {
                     return (
