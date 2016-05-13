300ee04cd9b9228113a609f34d01e9625bcab893
thomas <tquiviger@gmail.com>
2016-05-13 15:43:13 +0800

    Https

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
+var https = require('https');
 var express = require('express');
 var path = require('path');
-var webpack = require('webpack');
 var cors = require('cors')
 var axios = require('axios');
 var fs = require('fs');
@@ -13,11 +13,16 @@ var configuration = JSON.parse(fs.readFileSync(configurationFile));
 var _baseBusArrivalsUrl = 'http://datamall2.mytransport.sg/ltaodataservice/BusArrival?SST=True&BusStopID=';
 var _baseBusStationsUrl = configuration.elasticSearchUrl + '/sgbus/bus_station/';
 
-
 var static_path = path.join(__dirname, 'public');
-var isDevelopment = (process.env.NODE_ENV !== 'production');
 
 app.use(cors());
+
+https.createServer({
+      key: fs.readFileSync('/Users/thomas/sgbus/key.pem'),
+      cert: fs.readFileSync('/Users/thomas/sgbus/cert.pem')
+    }, app).listen(8080, function(){
+                          console.log("Express server listening on port " + 8080);
+                        });
 
 app.get('/api/bus_arrivals/:bus_stop_id', function (req, res) {
     axios.get(_baseBusArrivalsUrl + req.params.bus_stop_id, {
@@ -46,25 +51,4 @@ app.use(express.static(static_path))
         res.sendFile('index.html', {
             root: static_path
         });
-    }).listen(process.env.PORT || 8080, function (err) {
-    if (err) {
-        console.log(err)
-    }
-    ;
-    console.log('Listening at localhost:8080');
 });
-
-if (isDevelopment) {
-    var config = require('./webpack.config');
-    var WebpackDevServer = require('webpack-dev-server');
-
-    new WebpackDevServer(webpack(config), {
-        publicPath: config.output.publicPath,
-        hot: true
-    }).listen(3000, 'localhost', function (err, result) {
-        if (err) {
-            console.log(err)
-        }
-        console.log('Listening at localhost:3000');
-    });
-}
