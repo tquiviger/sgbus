79744a63b1210f0fbc809bc713718ef251b08d3b
thomas <tquiviger@gmail.com>
2016-06-09 16:33:34 +0800

    Responsive homepage

--- app/components/Home.js
+++ app/components/Home.js
@@ -11,11 +11,19 @@ var styles = {
     container: {
         height: 'auto',
         padding: 30,
-        textAlign: 'center'
+        textAlign: 'center',
+        display: 'flex',
+        align: 'middle',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     images: {
         width: 80,
-        padding: 10,
+        padding: 20,
+        marginRight: 30,
+        marginLeft: 30,
+        marginBottom: 10,
         borderRadius: '5px',
         boxShadow: '10px 10px 5px #888888',
         border: '3px solid #000'
@@ -37,38 +45,37 @@ var styles = {
 var Home = React.createClass({
     render: function () {
         return (
-            <div className="container-fluid" style={styles.container}>
-                <div className="row">
-                    <div className="col-xs-12 col-md-4" >
+            <div style={styles.container}>
+                <div>
+                    <div>
                         <Link to={'/buses'}>
                             <img src={busLogo} style={styles.images} alt="Bus logo"/>
                         </Link>
                         <h3 style={styles.caption}><i style={styles.icon} className="fa fa-search"/>Bus</h3>
                     </div>
-                    <div className="col-xs-12 col-md-3">
+                    <div >
                         <Link to={'/stations'}>
                             <img src={busStopLogo} style={styles.images} alt="Bus station logo"/>
                         </Link>
                         <h3 style={styles.caption}><i style={styles.icon} className="fa fa-search"/>Station
                         </h3>
                     </div>
-                    <div className="col-xs-12 col-md-4">
+
+                </div>
+                <div>
+                    <div >
                         <Link to={'/itineraries'}>
                             <img src={itineraryLogo} style={styles.images} alt="Itinerary logo"/>
                         </Link>
                         <h3 style={styles.caption}><i style={styles.icon} className="fa fa-search"/>Itinerary
                         </h3>
                     </div>
-                </div>
-                <div className="row">
-                    <div className="col-xs-12 col-md-4"></div>
-                    <div className="col-xs-12 col-md-3">
+                    <div>
                         <Link to={'/stats'}>
                             <img src={statsLogo} style={styles.images} alt="Statistics logo"/>
                         </Link>
                         <h3 style={styles.caption}>Statistics</h3>
                     </div>
-                    <div className="col-xs-12 col-md-4"></div>
                 </div>
             </div>
         )
